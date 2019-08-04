23cf529fc3cccaa7998562560c3b6759531b45fc
Ed Babcock <ed@edbabcock.com>
2019-08-04 14:48:39 -0500

    fix: eslint issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -7,5 +10,5 @@ export const Greetings = ({ name }) => <span>{`hello ${name}!`}</span>;
 
 // make sure to set the propTypes
 Greetings.propTypes = {
-    name: PropTypes.string
-}
+  name: PropTypes.string,
+};
